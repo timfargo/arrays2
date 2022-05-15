dc2161fa107df5943b54f9cc499ff089c00ecc52
timfargo <isalintimur981@gmail.com>
2022-05-15 19:17:00 +0300

    Initial commit

---
+++ src/sky/pro/Main.java
@@ -0,0 +1,118 @@
+package sky.pro;
+
+    public class Main {
+
+        public static void main(String[] args) {
+            //Задание 1
+//            int[] weight = new int[]{1, 2, 3};
+
+//            for (int i = 0; i <=weight.length - 1; i ++) {
+//                System.out.println(weight[i]);
+//                if (i != weight.length -1) System.out.println(",");
+
+//            float [] weight = new float[] {1.57f,7.654f,9.986f};
+
+//            for (int i = 0; i <= weight.length - 1; i ++){
+//                System.out.println(weight[i]);
+//            if (i != weight.length - 1) System.out.println(",");
+
+//            int [] weight = new int[]{12,11,10,9,8,7,6,5,4,3,2,1,0};
+
+//                   for (int i = 0; i <= weight.length - 1; i ++){
+//                System.out.println(weight[i]);
+//            if (i != weight.length - 1) System.out.println(",");
+
+            //Задание 2
+//        int[] weight = new int[3];
+//        weight[0] = 1;
+//        weight[1] = 2;
+//        weight[2] = 3;
+
+//        for (int i = 0; i < weight.length; i++) {
+//            if (i != weight.length - 1)
+//                System.out.print(weight[i] + ",");
+//            else
+//                System.out.print(weight[i]);
+//        }
+//        System.out.println();
+
+//        float[] weight = new float[]{1.57f,7.654f,9.986f};
+
+//        for (int i = 0; i < weight.length; i++) {
+//            if (i != weight.length - 1)
+//                System.out.print(weight[i] + ",");
+//            else
+//                System.out.print(weight[i]);
+            //       }
+//        System.out.println();
+//        int [] weight = new int[]{1,2,3,4,5,6,7,8,9,10,11,12};
+
+//        for (int i = 0; i < weight.length; i++) {
+//            if (i != weight.length - 1)
+//                System.out.print(weight[i] + ",");
+//            else
+//                System.out.print(weight[i]);
+//        }
+//        System.out.println();
+
+            //Задание 3
+//            int[] weight = new int[]{3,2,1};
+
+//            for (int i = 0; i < weight.length; i++) {
+//                if (i != weight.length - 1)
+//                    System.out.print(weight[i] + ",");
+//                else
+//                    System.out.print(weight[i]);
+//            }
+//            System.out.println();
+
+//            float [] weight = new float[]{9.986f,7.654f,1.57f};
+
+//            for (int i = 0; i < weight.length; i++) {
+//                if (i != weight.length - 1)
+//                    System.out.print(weight[i] + ",");
+//                else
+//                    System.out.print(weight[i]);
+//            }
+//            System.out.println();
+//            int [] weight = new int[]{12,11,10,9,8,7,6,5,4,3,2,1};
+
+//            for (int i = 0; i < weight.length; i++) {
+//                if (i != weight.length - 1)
+//                    System.out.print(weight[i] + ",");
+//                else
+//                    System.out.print(weight[i]);
+//            }
+//            System.out.println();
+
+            // Задание 4
+            int[] arr = new int[100];
+            for (int i = 2; i <= 100; i++) {
+                    System.out.println(i++);
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
